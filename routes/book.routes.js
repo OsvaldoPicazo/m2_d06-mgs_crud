e0c6a118b2ddd6b6c5ab77b65da98f312589cf53
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-06 14:30:56 +0200

    added function to edit book entry

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -7,23 +7,103 @@ const router = express.Router();
 // ********* require Book model in order to use it *********
 const Book = require('../models/Book.model');
 
-// ****************************************************************************************
-// GET route to display all the books
-// ****************************************************************************************
+// When routing, use the inverted pyramid rule: route from the most specific to the least specific route
 
-router.get('/books', (req, res) => {
-  Book.find()
-   // You have to continue coding the route
-});
+router.get(
+  "/new",
+  (req, res)=>{
+  res.render("new-book")
+})
+
+// This is the twin routes way that we saw on the express routes lesson
+/* router.route("/new")
+.get((req, res)=>{
+  res.render("new-book")
+})
+.post((req, res)=>{
+  // Create entity here
+}) */
+
+// but instead we will follow a strong convention a post data no using twin routes
 
 // ****************************************************************************************
 // GET route for displaying the book details page
 // ****************************************************************************************
+router.get("/:id", (req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => {
+      res.render('book-details', book)
+    })
+})
 
-router.get('/books/:id', (req, res) => {
-  const id = req.params.id;
-  Book.findById(id)
+// ****************************************************************************************
+// GET route for deleting a book
+// ****************************************************************************************
+
+// Problem: HTML only takes GET and POST request:
+/*
+router.delete("/:id", (req, res)=>{
+  res.send(`Deleted book ${req.params.id}`)
+})
+*/
+
+// Solution: implement the delete method as an URL path and delete with mongoose method
+
+router.get("/:id/delete",(req, res)=>{
+  Book.findByIdAndDelete(req.params.id)
+  .then(deletedBook => res.redirect("/books"))
+  .catch(error=> console.log(error))
+})
+
+// ****************************************************************************************
+// GET route for editing a book
+// ****************************************************************************************
+
+// Problem:The HTML interface does not allow the delete verb to be sent in a request
+/*
+router.put("/:id", (req, res)=>{
+  res.send(`Edited book ${req.params.id}`)
+})
+*/
+
+//Solution
+router.route("/:id/edit")
+.get((req, res)=>{
+  Book.findById(req.params.id)
+  .then(book => res.render("book-edit", book))
+  // {data} curly braces only needed when passing an array
+})
+.post((req, res)=>{
+  const {title, author, description, rating} = req.body
+  Book.findByIdAndUpdate(
+    req.params.id,
+    {title, author, description, rating}
+  )
+  .then(updateBook => res.redirect(`/books/${req.params.id}`))
+  .catch(error => console.log(error))
+})
+
+// ****************************************************************************************
+// GET route to display all the books
+// ****************************************************************************************
+  
+
+// This is the base path of the DB entity
+router.get('/', 
+(req, res) => {
+  Book.find()
+  .then(books => {
+    res.render('books-list', {books})
+  })
    // You have to continue coding the route
 });
+  
+router.post(
+  "/",
+  (req, res)=>{
+  const {title, author, description, rating} = req.body
+  Book.create({title, author, description, rating})
+  .then(newBook => res.redirect("/books/"))
+})
 
 module.exports = router;
